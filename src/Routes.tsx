0c4a5b2857f0783eaf6f114dc156ded23983f957
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-11 14:20:24 +0000

    Seutp initial router configurations

---
+++ src/Routes.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {
+  Route,
+  IndexRoute
+} from 'react-router';
+
+import App from 'View/App';
+
+export default (
+  <Route path="/"
+         component={App}>
+  </Route>
+);
