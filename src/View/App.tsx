2ac843dd0b4d31499bbdce402ea5a168f8719f91
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-11 14:20:16 +0000

    Initial App view

---
+++ src/View/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class App extends React.Component<any, any>
+{
+  render()
+  {
+    return (
+      <div>
+        {'Hello World!'}
+      </div>
+    );
+  }
+};
+
+export default App;
