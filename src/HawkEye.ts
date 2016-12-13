8976a0e22d39a11de6476cc440edb59bf48cacc3
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-13 20:03:30 +0000

    Service bindings.

--- src/HawkEye.ts
+++ src/HawkEye.ts
@@ -1,11 +1,19 @@
-import { syncHistoryWithStore } from 'react-router-redux';
-import { hashHistory } from 'react-router';
-
+import InstanceCache from 'Core/InstanceCache';
 import reducers from 'Reducers/Index';
 import configureStore from 'Core/ConfigureStore';
 import { renderApplication } from 'Core/Renderer';
+
+import RequestFactory from 'Core/RequestFactory';
+import GitHub from 'GitHub/GitHub';
+
 import routes from 'Routes';
 
+import { syncHistoryWithStore } from 'react-router-redux';
+import { hashHistory } from 'react-router';
+
+/**
+ * @todo: This needs to change. A lot.
+ */
 class HawkEye
 {
   constructor()
@@ -14,6 +22,11 @@ class HawkEye
     const history = syncHistoryWithStore(hashHistory, store);
 
     const renderTarget = document.getElementById('root');
+
+    // Services
+    InstanceCache
+      .addInstance<IGitHub>('IGitHub', new GitHub(
+                                         new RequestFactory('GITHUB HOST')));
 
     // Render the application
     renderApplication(renderTarget, store, history, routes);
