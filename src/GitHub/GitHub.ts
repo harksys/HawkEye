fdd925ae82d450e0a20f72eb4e2cc0ca9fb1c351
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-13 19:51:29 +0000

    Start GitHub.ts

---
+++ src/GitHub/GitHub.ts
@@ -0,0 +1,11 @@
+///<refernce path="../Core/Interfaces/IRequestFactory" />
+
+class GitHub
+{
+  private requestFactory: IRequestFactory;
+
+  constructor(requestFactory: IRequestFactory)
+  {
+    this.requestFactory = requestFactory;
+  }
+};
