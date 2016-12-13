5503449de356a33489ffdb4d8cbeecf9e0fc1b69
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-13 19:58:20 +0000

    SettingsAddAccount view start

---
+++ src/View/Settings/AddAccount.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class SettingsAddAccount extends React.Component<any, any>
+{
+  render()
+  {
+    return (
+      <div>
+        {'Settings Add Account'}
+      </div>
+    );
+  }
+};
+
+export default SettingsAddAccount;
