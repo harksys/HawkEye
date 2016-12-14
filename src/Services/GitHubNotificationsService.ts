83bb1dbc1cc62cf953a2c35feaf822f8a44ab798
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-14 16:41:10 +0000

    Initial GitHub Services

--- src/Services/GitHubNotificationsService.ts
+++ src/Services/GitHubNotificationsService.ts
@@ -1,8 +1,16 @@
 ///<reference path="./Interfaces/IGitHubNotificationsService.ts" />
+///<reference path="../GitHub/Interfaces/IGitHub.ts" />
+
+import InstanceCache from 'Core/InstanceCache';
 
 class GitHubNotificationsService implements IGitHubNotificationsService
 {
+  private gitHub: IGitHub;
 
+  constructor()
+  {
+    this.gitHub = InstanceCache.getInstance<IGitHub>('IGitHub');
+  }
 };
 
 export default GitHubNotificationsService;
--- src/Services/GithubAccountsService.ts
+++ src/Services/GithubAccountsService.ts
@@ -1,8 +1,16 @@
 ///<reference path="./Interfaces/IGitHubAccountsService.ts" />
+///<reference path="../GitHub/Interfaces/IGitHub.ts" />
+
+import InstanceCache from 'Core/InstanceCache';
 
 class GitHubAccountsService implements IGitHubAccountsService
 {
+  private gitHub: IGitHub;
 
+  constructor()
+  {
+    this.gitHub = InstanceCache.getInstance<IGitHub>('IGitHub');
+  }
 };
 
 export default GitHubAccountsService;
