c98906e0d1bd2649992a66e9f236acdfcfc9e863
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-14 19:24:39 +0000

    Test getElectron method future GitHub auth work.

--- src/View/Settings/Index.tsx
+++ src/View/Settings/Index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { getElectron } from 'Helpers/System/Electron';
 
 class SettingsIndex extends React.Component<any, any>
 {
@@ -7,8 +8,40 @@ class SettingsIndex extends React.Component<any, any>
     return (
       <div>
         {'Settings'}
+        <a href="#"
+           onClick={this.handleClick.bind(this)}>
+          {'Add User'}
+        </a>
       </div>
     );
+  }
+
+  handleClick(e)
+  {
+    e.preventDefault();
+
+    let BrowserWindow = getElectron().remote.BrowserWindow;
+    let authWindow    = new BrowserWindow({
+      width : 800,
+      height : 600,
+      show : true,
+      webPreferences : {
+        nodeIntegration : false
+      }
+    });
+
+    var url = 'https://harksys.com';
+    authWindow.loadURL(url);
+
+    authWindow.on('close', () =>
+    {
+      authWindow.destroy();
+    });
+
+    authWindow.webContents.on('will-navigate', (e, u) =>
+    {
+      console.log(u);
+    });
   }
 };
 
