0056c91ad2f46e883a497f21d70ede53e5bfb77d
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-15 21:36:03 +0000

    Switch Routes to object-form

---
+++ src/Routes.ts
@@ -0,0 +1,9 @@
+import Index from 'View/Index';
+import SettingsIndex from 'View/Settings/Index';
+
+const routes: ReactRouter.PlainRoute[] = [{
+  path : 'settings',
+  component : SettingsIndex
+}]
+
+export default routes;
