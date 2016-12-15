a9020a2fd357f8fd45a9f8d87347476af814ec7e
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-15 19:45:56 +0000

    IStateSettings Interfaces

--- src/Interfaces/IState/IStateSettings.ts
+++ src/Interfaces/IState/IStateSettings.ts
@@ -1,5 +1,22 @@
 
 interface IStateSettings
 {
+  authentication: IStateSettingsAuthentication;
+
+  accountSettings: IStateSettingsAccounts;
+};
+
+interface IStateSettingsAuthentication
+{
+  isAuthenticating: boolean;
+};
+
+interface IStateSettingsAccounts
+{
+  [accountId: string]: IStateSettingsAccountsSettingsItem;
+};
+
+interface IStateSettingsAccountsSettingsItem
+{
 
 };
