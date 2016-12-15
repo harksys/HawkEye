2266b784061ed9a1bdb90a047de9797d7630298a
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-15 13:27:49 +0000

    GitHub Scopes object constants

--- src/Constants/Services/GitHub.ts
+++ src/Constants/Services/GitHub.ts
@@ -1,2 +1,21 @@
 
-export const gitHubApiUrl = 'https://api.github.com';
+export const gitHubApiUrl: string = 'https://api.github.com';
+
+
+/*
+ * GitHub Scope Strings
+ *
+ * Most, but cut down to never allow access to sensitive things.
+ * For example, delete_repo etc.
+ */
+export const gitHubScopes = {
+  user           : 'user',
+  userEmail      : 'user:email',
+  userFollow     : 'user:follow',
+  publicRepo     : 'public_repo',
+  repo           : 'repo',
+  repoDeployment : 'repo_deployment',
+  repoStatus     : 'repo:status',
+  gist           : 'gist',
+  notifications  : 'notifications'
+};
