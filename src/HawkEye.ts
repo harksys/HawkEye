c080bf8abe231d610d4f8f36b2d1caf7f9535343
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-15 17:27:32 +0000

    Bind GitHub Services to InstanceCache

--- src/HawkEye.ts
+++ src/HawkEye.ts
@@ -6,6 +6,10 @@ import { renderApplication } from 'Core/Renderer';
 import RequestFactory from 'Core/RequestFactory';
 import GitHub from 'GitHub/GitHub';
 
+import GitHubAccountsService from 'Services/GitHubAccountsService';
+import GitHubAuthenticationService from 'Services/GitHubAuthenticationService';
+import GitHubNotificationsService from 'Services/GitHubNotificationsService';
+
 import { gitHubApiUrl } from 'Constants/Services/GitHub';
 
 import routes from 'Routes';
@@ -25,10 +29,18 @@ class HawkEye
 
     const renderTarget = document.getElementById('root');
 
+    // GitHub
+    const gitHubService = new GitHub(new RequestFactory(gitHubApiUrl));
+
     // Services
     InstanceCache
-      .addInstance<IGitHub>('IGitHub', new GitHub(
-                                         new RequestFactory(gitHubApiUrl)));
+      .addInstance<IGitHub>('IGitHub', gitHubService)
+      .addInstance<IGitHubAccountsService>('IGitHubAccountsService',
+                                                 new GitHubAccountsService(gitHubService))
+      .addInstance<IGitHubAuthenticationService>('IGitHubAuthenticationService',
+                                                 new GitHubAuthenticationService(gitHubService))
+      .addInstance<IGitHubNotificationsService>('IGitHubNotificationsService',
+                                                 new GitHubNotificationsService(gitHubService))
 
     // Render the application
     renderApplication(renderTarget, store, history, routes);
