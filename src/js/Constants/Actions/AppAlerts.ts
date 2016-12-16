6f08c12c852a58e3666721f983371c4c99052f07
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-16 21:43:39 +0000

    AppAlert Action Constants

--- src/js/Constants/Actions/AppAlerts.ts
+++ src/js/Constants/Actions/AppAlerts.ts
@@ -1,6 +1,10 @@
 
 const AppAlertsActions = {
-
+  ADD_APP_ALERT       : 'ADD_APP_ALERT',
+  REMOVE_APP_ALERT    : 'REMOVE_APP_ALERT',
+  SHOW_APP_ALERT      : 'SHOW_APP_ALERT',
+  HIDE_APP_ALERT      : 'HIDE_APP_ALERT',
+  HIDE_ALL_APP_ALERTS : 'HIDE_ALL_APP_ALERTS'
 };
 
 export default AppAlertsActions;
