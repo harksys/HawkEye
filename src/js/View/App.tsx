373b9d87a9c8759e4307444382022201c22ceef6
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-16 19:57:34 +0000

    Left bar for App

--- src/js/View/App.tsx
+++ src/js/View/App.tsx
@@ -9,12 +9,19 @@ class App extends React.Component<any, any>
   render()
   {
     return (
-      <div>
-        <AppAlerts />
-        <Link to="/settings">
-          {'View Settings'}
-        </Link>
-        {this.props.children}
+      <div className="width--full height--full">
+        <div className="hard-left hard-left--delta">
+          <div className="hard-left__left bg--dark-grey">
+
+          </div>
+          <div className="hard-left__content">
+            <AppAlerts />
+            <Link to="/settings">
+              {'View Settings'}
+            </Link>
+            {this.props.children}
+          </div>
+        </div>
       </div>
     );
   }
