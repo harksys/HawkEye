6869d6f1f0c1cbaea7cb9acababe7181f9623903
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-17 14:47:08 +0000

    Add back-link to ViewBar

--- src/js/View/Components/ViewBar/Index.tsx
+++ src/js/View/Components/ViewBar/Index.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 
+import { Link } from 'react-router';
+import { Icon } from 'View/Ui/Index';
+
 interface IViewBarProps
 {
   title: string;
+
+  backLink?: string;
 };
 
 class ViewBar extends React.Component<IViewBarProps, any>
@@ -12,7 +17,17 @@ class ViewBar extends React.Component<IViewBarProps, any>
     return (
       <div className="hard-top hard-top--delta">
         <div className="hard-top__top bg--light-grey">
-          <p>{this.props.title}</p>
+          <div className="view-bar">
+            {typeof this.props.backLink === 'string'
+              ? <Link to={this.props.backLink}
+                      className="view-bar__back-link">
+                  <Icon icon="angle-left" />
+                </Link>
+              : undefined}
+            <p className="view-bar__title">
+              {this.props.title}
+            </p>
+          </div>
         </div>
         <div className="hard-top__content">
           {this.props.children}
