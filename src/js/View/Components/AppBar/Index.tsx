571cf837a8f05b9d2b79345f4d448b44530286c0
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-17 11:46:27 +0000

    Initial work on AppBar

--- src/js/View/Components/AppBar/Index.tsx
+++ src/js/View/Components/AppBar/Index.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 
-import { Icon } from 'View/Ui/Index';
+import {
+  Icon,
+  ProfilePicture
+} from 'View/Ui/Index';
 
 interface IAppBarProps
 {
+  app?: IStateApp;
 
+  accounts?: IStateAccountsAccount[];
 };
 
 class AppBar extends React.Component<IAppBarProps, any>
@@ -12,12 +18,30 @@ class AppBar extends React.Component<IAppBarProps, any>
   render()
   {
     return (
-      <div>
-        {'App Bar'}
-        <Icon icon="world" />
+      <div className="hard-bottom hard-bottom--delta">
+        <div className="hard-bottom__content">
+          {this.props.accounts
+               .map(account =>
+               (
+                 <div key={account.gitHubUser.id}
+                      className="soft-zeta">
+                   <ProfilePicture picture={account.gitHubUser.avatarUrl} />
+                 </div>
+               ))}
+        </div>
+        <div className="hard-bottom__bottom">
+          <Icon icon="world" />
+        </div>
       </div>
     );
   }
 };
 
-export default AppBar;
+export default connect(
+  (state: IState, props: IAppBarProps) => ({
+    app      : state.app,
+    accounts : Object.keys(state.accounts)
+                     .map(id => state.accounts[id])
+
+  })
+)(AppBar as any);
