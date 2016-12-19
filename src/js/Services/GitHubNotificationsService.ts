b58213811b6fa8af5fc27ceaf649583e07f46e83
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-19 20:22:17 +0000

    GitHubNotificationsService.getNotifications

--- src/js/Services/GitHubNotificationsService.ts
+++ src/js/Services/GitHubNotificationsService.ts
@@ -9,6 +9,30 @@ class GitHubNotificationsService implements IGitHubNotificationsService
   {
     this.gitHub = gitHub;
   }
+
+  /**
+   * @param  {string} token
+   * @param  {boolean} all?
+   * @param  {boolean} participating?
+   * @param  {string} since?
+   * @param  {string} before?
+   * @returns Promise
+   */
+  public getNotifications = (token: string,
+                             all?: boolean,
+                             participating?: boolean,
+                             since?: string,
+                             before?: string): Promise<any[]> =>
+  {
+    return new Promise((resolve, reject) =>
+    {
+      this.gitHub
+          .activity
+          .getNotifications(token, all, participating, since, before)
+          .then(res => resolve(res),
+                err => reject(err));
+    });
+  };
 };
 
 export default GitHubNotificationsService;
--- src/js/Services/Interfaces/IGitHubNotificationsService.ts
+++ src/js/Services/Interfaces/IGitHubNotificationsService.ts
@@ -1,5 +1,9 @@
 
 interface IGitHubNotificationsService
 {
-
+  getNotifications(token: string,
+                   all?: boolean,
+                   participating?: boolean,
+                   since?: string,
+                   before?: string): Promise<any[]>;
 };
