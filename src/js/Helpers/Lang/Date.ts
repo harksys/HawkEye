3913de252fd67773df954219d95e541e7aaee4c5
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-20 17:34:13 +0000

    Initial date helpers

--- src/js/Helpers/Lang/Date.ts
+++ src/js/Helpers/Lang/Date.ts
@@ -1,3 +1,5 @@
+import * as Moment from 'moment';
+
 import { cronPeriods } from 'Constants/Lang/Date';
 
 /**
@@ -7,4 +9,41 @@ import { cronPeriods } from 'Constants/Lang/Date';
 export function getCronPeriodByName(name: string): string
 {
   return cronPeriods[name] || null;
+};
+
+/**
+ * @param  {} date?
+ * @param  {} format='YYYY-MM-DD'
+ * @returns string
+ */
+export function formatDate(date?, format = 'YYYY-MM-DD'): string
+{
+  return Moment(date).format(format);
+};
+
+/**
+ * @param  {string|moment.Moment} date
+ * @returns moment
+ */
+export function toUtc(date: string | moment.Moment): moment.Moment
+{
+  return Moment(date).utc();
+};
+
+/**
+ * @param  {} date?
+ * @returns string
+ */
+export function formatDateAsUTC(date?): string
+{
+  return formatDate(date, 'YYYY-MM-DDTHH:mm:ss') + 'Z';
+};
+
+/**
+ * @param  {} date?
+ * @returns moment
+ */
+export function convertUtcToLocal(date?): moment.Moment
+{
+  return Moment(date).utc().local();
 };
