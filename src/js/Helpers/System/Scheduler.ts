3a4cb2711519de5f98f6f2e978644ff510852664
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-20 21:56:13 +0000

    Scheduler helpers to handle configuring jobs, polling of notifications etc. All comes together.

--- src/js/Helpers/System/Scheduler.ts
+++ src/js/Helpers/System/Scheduler.ts
@@ -1,7 +1,11 @@
 import InstaceCache from 'Core/InstanceCache';
 
-import { poll } from 'Actions/Notifications';
+import { pollSinceNotifications } from 'Actions/Notifications';
 
+import {
+  isPolling,
+  getLastPoll
+} from 'Helpers/Models/App';
 import {
   getAccountIds,
   getAccountToken
@@ -9,23 +13,53 @@ import {
 import { dispatch } from 'Helpers/State/Store';
 import { getCronPeriodByName } from 'Helpers/Lang/Date';
 
+/**
+ * @param  {string} pollPeriod
+ */
 export function configurePollingScheduler(pollPeriod: string)
 {
   let scheduler = InstaceCache.getInstance<IScheduler>('IScheduler');
 
+  /*
+   * Clear all the current jobs. We'll reconfigure them below.
+   */
   scheduler.clearAllJobs();
 
+  /*
+   * Setup the job for polling
+   */
   let name = scheduler.scheduleJob(getCronPeriodByName(pollPeriod), {
     accountIds : getAccountIds()
   }, () =>
   {
+    if (isPolling()) {
+      return;
+    }
+
+    /*
+     * Get the parameters we passed for this job
+     */
     let params = scheduler.getJobParameters(name);
+    if (typeof params.accountIds === 'undefined') {
+      return;
+    }
 
     pollingMethod(params.accountIds);
   });
 };
 
+/**
+ * @param  {string[]} accountIds
+ */
 export function pollingMethod(accountIds: string[])
 {
-  accountIds.forEach(id => dispatch(poll(getAccountToken(id))));
+  let lastPoll = getLastPoll();
+
+  /*
+   * For each of the accounts, poll for
+   * notifications since we last polled.
+   */
+  accountIds.forEach(id => dispatch(pollSinceNotifications(
+    id, getAccountToken(id), lastPoll, true
+  )));
 };
