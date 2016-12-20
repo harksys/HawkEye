233a88eea11c71005aa03d8ffb6097520bf8a190
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-20 17:02:33 +0000

    Configure scheduling when the value is updated

--- src/js/Actions/UIActions/Settings.ts
+++ src/js/Actions/UIActions/Settings.ts
@@ -1,12 +1,18 @@
+import InstanceCache from 'Core/InstanceCache';
+
 import { updatePollPeriod } from 'Actions/Settings';
+import { configurePollingScheduler } from 'Helpers/System/Scheduler';
 
 /**
  * @param  {string} pollPeriod
  */
 export function configurePollPeriod(pollPeriod: string)
 {
+  let scheduler = InstanceCache.getInstance<IScheduler>('IScheduler');
+
   return dispatch =>
   {
     dispatch(updatePollPeriod(pollPeriod));
+    configurePollingScheduler(pollPeriod);
   };
 };
