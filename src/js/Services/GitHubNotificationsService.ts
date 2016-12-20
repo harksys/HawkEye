3cae2885fec79863d9856f76042822a5cf478a96
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-20 18:52:29 +0000

    GitHubNotificationsService methods to recursively go get notifications!

--- src/js/Services/GitHubNotificationsService.ts
+++ src/js/Services/GitHubNotificationsService.ts
@@ -1,6 +1,9 @@
 ///<reference path="./Interfaces/IGitHubNotificationsService.ts" />
 ///<reference path="../GitHub/Interfaces/IGitHub.ts" />
 
+import * as Async from 'async';
+import * as concat from 'lodash/concat';
+
 class GitHubNotificationsService implements IGitHubNotificationsService
 {
   private gitHub: IGitHub;
@@ -20,7 +23,6 @@ class GitHubNotificationsService implements IGitHubNotificationsService
    */
   public getNotifications = (token: string,
                              page: number = 1,
-                             perPage: number = 100,
                              all?: boolean,
                              participating?: boolean,
                              since?: string,
@@ -30,11 +32,102 @@ class GitHubNotificationsService implements IGitHubNotificationsService
     {
       this.gitHub
           .activity
-          .getNotifications(token, page, perPage, all, participating, since, before)
+          .getNotifications(token, page, all, participating, since, before)
           .then(res => resolve(res),
                 err => reject(err));
     });
   };
+
+  /**
+   * @param  {string} token
+   * @param  {string} since
+   * @param  {boolean=true} all
+   * @returns Promise
+   */
+  public getAllNotificationsSince = (token: string,
+                                     since: string,
+                                     all: boolean = true): Promise<any[]> =>
+  {
+    return this.recursiveGetNotifications(token, all, since);
+  }
+
+  /**
+   * @param  {string} token
+   * @param  {string} before
+   * @param  {boolean=true} all
+   * @returns Promise
+   */
+  public getAllNotificationsBefore = (token: string,
+                                      before: string,
+                                      all: boolean = true): Promise<any[]> =>
+  {
+    return this.recursiveGetNotifications(token, all, undefined, before);
+  };
+
+  /**
+   * @param  {string} token
+   * @param  {boolean=false} all
+   * @param  {string} since?
+   * @param  {string} before?
+   * @returns Promise
+   */
+  private recursiveGetNotifications = (token: string,
+                                       all: boolean = false,
+                                       since?: string,
+                                       before?: string): Promise<any[]> =>
+  {
+    /*
+     * This method is private because it should
+     * never see the light of day.
+     * However it does work, and it'll help.
+     */
+    return new Promise(resolve =>
+    {
+      let notifications = [];
+      let currentPage   = 1;
+      let keepPolling   = true;
+
+      Async.doWhilst(cb =>
+      {
+        this.getNotifications(token, currentPage, false, false, since, before)
+            .then(res =>
+            {
+              /*
+               * If the request fails, then cancel and callback.
+               */
+              if (!Array.isArray(res)) {
+                keepPolling = false;
+                cb();
+                return;
+              }
+
+              /*
+               * Apply the new notifications.
+               * As we get 50 at a time, if we get only
+               * 49 then we get to the end of the requests we
+               * need to make. So stop here.
+               */
+              notifications = concat(notifications, res);
+              if (res.length <= 49) {
+                keepPolling = false;
+                cb();
+                return;
+              }
+
+              /*
+               * If we get here, theres more requests to make.
+               * So carry on, to the next page!
+               */
+              currentPage = currentPage + 1;
+              cb();
+            });
+      }, () => keepPolling === true, () =>
+      {
+        // Return the total notifications!
+        resolve(notifications);
+      });
+    });
+  }
 };
 
 export default GitHubNotificationsService;
--- src/js/Services/Interfaces/IGitHubNotificationsService.ts
+++ src/js/Services/Interfaces/IGitHubNotificationsService.ts
@@ -3,9 +3,12 @@ interface IGitHubNotificationsService
 {
   getNotifications(token: string,
                    page?: number,
-                   perPage?: number,
                    all?: boolean,
                    participating?: boolean,
                    since?: string,
                    before?: string): Promise<any[]>;
+
+  getAllNotificationsSince(token: string, since: string, all?: boolean): Promise<any[]>;
+
+  getAllNotificationsBefore(token: string, before: string, all?: boolean): Promise<any[]>;
 };
