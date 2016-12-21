88976b7cb83187bc208964f3f31dcdbdf034997e
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-21 19:29:45 +0000

    Scroll the account list

--- src/js/View/Components/AppBar/Index.tsx
+++ src/js/View/Components/AppBar/Index.tsx
@@ -5,9 +5,10 @@ import { switchAccount } from 'Actions/UIActions/App';
 import { dispatch } from 'Helpers/State/Store';
 
 import {
-  Icon,
   Btn,
+  Icon,
   BtnTo,
+  Scroll,
   CenteredBox,
   ProfilePicture
 } from 'View/Ui/Index';
@@ -26,22 +27,24 @@ class AppBar extends React.Component<IAppBarProps, any>
     return (
       <div className="hard-bottom hard-bottom--delta">
         <div className="hard-bottom__content">
-          {this.props.accounts
-               .map(account =>
-               (
-                 <div key={account.gitHubUser.id}
-                      className="soft-zeta hard--bottom">
-                   <Btn onClick={() => dispatch(switchAccount(account.gitHubUser.id))}
-                        className={'btn--square-delta'
-                                      + (this.props.app.currentAccountId != account.gitHubUser.id
-                                          ? ' btn--dark-grey-brand'
-                                          : '')}>
-                     <div className="soft-kappa">
-                       <ProfilePicture picture={account.gitHubUser.avatarUrl} />
-                     </div>
-                   </Btn>
-                 </div>
-               ))}
+          <Scroll>
+            {this.props.accounts
+                .map(account =>
+                (
+                  <div key={account.gitHubUser.id}
+                        className="soft-zeta hard--bottom">
+                    <Btn onClick={() => dispatch(switchAccount(account.gitHubUser.id))}
+                          className={'btn--square-delta'
+                                        + (this.props.app.currentAccountId != account.gitHubUser.id
+                                            ? ' btn--dark-grey-brand'
+                                            : '')}>
+                      <div className="soft-kappa">
+                        <ProfilePicture picture={account.gitHubUser.avatarUrl} />
+                      </div>
+                    </Btn>
+                  </div>
+                ))}
+          </Scroll>
         </div>
         <div className="hard-bottom__bottom bg--dark-grey">
           <div className="soft-zeta">
