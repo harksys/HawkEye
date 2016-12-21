a950414326a419c10ad597991476c4c4fbf18ded
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-21 13:23:59 +0000

    Index -> temp number of notiifcations

--- src/js/View/Index.tsx
+++ src/js/View/Index.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+
+import * as concat from 'lodash/concat';
 
 import ViewBar from 'View/Components/ViewBar/Index';
 
-class Index extends React.Component<any, any>
+interface IAppIndexProps
+{
+  notifications: IStateNotifications;
+};
+
+class Index extends React.Component<IAppIndexProps, any>
 {
   render()
   {
+    let notifications: IGitHubNotification[] = [];
+    Object.keys(this.props.notifications)
+        .forEach(accId =>
+        {
+          Object.keys(this.props.notifications[accId])
+                .forEach(repoId => {
+                  Object.keys(this.props.notifications[accId][repoId])
+                        .forEach(notificationId => {
+                          notifications.push(this.props.notifications[accId][repoId][notificationId]);
+                        })
+                })
+        });
+
     return (
       <ViewBar title="Notifications">
         <div className="hideable-left">
@@ -14,6 +35,7 @@ class Index extends React.Component<any, any>
           </div>
           <div className="hideable-left__content">
             <p>{'Notifications go here'}</p>
+            {'L: ' + notifications.length}
           </div>
         </div>
       </ViewBar>
@@ -21,4 +43,8 @@ class Index extends React.Component<any, any>
   }
 };
 
-export default Index;
+export default connect(
+  (state: IState, props: IAppIndexProps) => ({
+    notifications : state.notifications
+  })
+)(Index);
