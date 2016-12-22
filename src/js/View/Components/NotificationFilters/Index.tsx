12053a29dc699f14db4adc6c28eb86b4433d05b1
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-22 16:52:56 +0000

    Format names and classes for filters

--- src/js/View/Components/NotificationFilters/Index.tsx
+++ src/js/View/Components/NotificationFilters/Index.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 
+import {
+  getNotificationReasonPrettyName,
+  getNotificationSubjectPrettyName
+} from 'Helpers/Services/GitHub';
 import { createGitHubNotificationFilterSet } from 'Helpers/Models/GitHubNotificationFilterSet';
 
 import NotificationFilterStringFilter from './NotificationFilterStringFilter';
 import NotificationFilterRepositoryFilter from './NotificationFilterRepositoryFilter';
+
+import { Scroll } from 'View/Ui/Index';
 
 interface INotificationFiltersProps
 {
@@ -17,19 +23,19 @@ class NotificationFilters extends React.Component<INotificationFiltersProps, any
     let filterSet = createGitHubNotificationFilterSet(this.props.notifications);
 
     return (
-      <div className="soft-delta">
+      <Scroll>
         <div className="grid">
           {filterSet.subjectTypes.length > 0
             ? <NotificationFilterStringFilter stringFilters={filterSet.subjectTypes}
-                                              className="grid__item one-whole push-delta--bottom"
+                                              className="grid__item one-whole"
                                               getTitle={() => 'Subjects'}
-                                              getFilterTitle={filter => filter.name} />
+                                              getFilterTitle={filter => getNotificationSubjectPrettyName(filter.name)} />
             : undefined}
           {filterSet.reasonTypes.length > 0
             ? <NotificationFilterStringFilter stringFilters={filterSet.reasonTypes}
-                                              className="grid__item one-whole push-delta--bottom"
+                                              className="grid__item one-whole"
                                               getTitle={() => 'Reasons'}
-                                              getFilterTitle={filter => filter.name} />
+                                              getFilterTitle={filter => getNotificationReasonPrettyName(filter.name)} />
             : undefined}
           {filterSet.repositories.length > 0
             ? <NotificationFilterRepositoryFilter repositoryFilters={filterSet.repositories}
@@ -37,8 +43,8 @@ class NotificationFilters extends React.Component<INotificationFiltersProps, any
                                                   getTitle={() => 'Repositories'}
                                                   getFilterTitle={filter => filter.repository.fullName.toLowerCase()} />
             : undefined}
-          </div>
-      </div>
+        </div>
+      </Scroll>
     );
   }
 };
