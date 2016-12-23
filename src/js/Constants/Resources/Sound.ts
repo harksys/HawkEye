93150415ce7d49af93c3786a0ca7b46c13ebae4a
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-23 11:03:06 +0000

    Sound constants to map to resources

---
+++ src/js/Constants/Resources/Sound.ts
@@ -0,0 +1,6 @@
+
+export const soundClipPaths = {
+  harkSuccess  : './resources/audio/hark-success.mp3',
+  harkError    : './resources/audio/hark-error.mp3',
+  harkNewItems : './resources/audio/hark-new-items.mp3'
+};
