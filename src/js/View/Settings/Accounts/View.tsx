ada6dfdb168edb9a73bbf3c9bfdeb912e41bdf4c
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-23 20:08:42 +0000

    Start account settings page

--- src/js/View/Settings/Accounts/View.tsx
+++ src/js/View/Settings/Accounts/View.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 import { dispatch } from 'Helpers/State/Store';
+import { getAccount } from 'Helpers/Models/Accounts';
 import { removeAccount } from 'Actions/UIActions/Accounts';
 
 import ViewBar from 'View/Components/ViewBar/Index';
@@ -21,17 +22,44 @@ class ViewAccountSettings extends React.Component<IViewAccountSettingsProps, any
 {
   render()
   {
+    let account = getAccount(this.props.params.accountId);
+
     return (
       <ViewBar title="Account Settings"
                backLink="/settings">
         <Scroll>
           <div className="soft-delta">
             <div className="grid">
-              <div className="grid__item one-whole">
-                <Btn className="btn--error"
-                    onClick={() => dispatch(removeAccount(this.props.params.accountId))}>
-                  Remove Account
-                </Btn>
+              <div className="grid__item one-quarter mobile-display--hidden"></div>
+              <div className="grid__item one-half mobile-one-whole">
+                <div className="grid">
+                  <div className="grid__item one-whole push-delta--bottom">
+                    <div className="bg--dark-grey-epsilon soft-zeta round">
+                      <div className="grid">
+                        <div className="grid__item one-third">
+                          <img src={account.gitHubUser.avatarUrl}
+                               className="profile-picture img-scale" />
+                        </div>
+                        <div className="grid__item two-thirds">
+                          <h4 className="text--white">{account.gitHubUser.name}</h4>
+                          <p className="text--white text--zeta">{'@' + account.gitHubUser.username}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="grid__item one-whole">
+                    <Btn className="push-delta--bottom">
+                      {'Update Details'}
+                    </Btn>
+                    <Btn className="btn--error push-delta--bottom">
+                      {'Clear Current Notifications'}
+                    </Btn>
+                    <Btn className="btn--error"
+                        onClick={() => dispatch(removeAccount(this.props.params.accountId))}>
+                      Remove Account
+                    </Btn>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
