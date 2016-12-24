534798b893902e593d9285e7b381b3181833a5bf
Andrew Hathaway <andrew@andrewhathaway.net>
2016-12-24 12:34:20 +0000

    Electron helpers to get a new menu and menu items set

--- src/js/Helpers/System/Electron.ts
+++ src/js/Helpers/System/Electron.ts
@@ -9,3 +9,24 @@ export function getElectron(): Electron.ElectronMainAndRenderer
 {
   return windowRequire('electron') as Electron.ElectronMainAndRenderer;
 };
+
+/**
+ * @returns Electron
+ */
+export function getNewRemoteElectronMenu(): Electron.Menu
+{
+  let electron = getElectron();
+
+  return new electron.remote.Menu();
+};
+
+/**
+ * @param  {Electron.MenuItemOptions} opts
+ * @returns Electron
+ */
+export function getNewRemoteElectronMenuItem(opts: Electron.MenuItemOptions): Electron.MenuItem
+{
+  let electron = getElectron();
+
+  return new electron.remote.MenuItem(opts);
+};
