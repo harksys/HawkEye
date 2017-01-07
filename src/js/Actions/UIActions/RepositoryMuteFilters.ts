8081e3239e47b790877e998c54354ebda2ba30ee
Andrew Hathaway <andrew@andrewhathaway.net>
2017-01-07 18:10:53 +0000

    Initial UI Actions to create a repository mute filter

---
+++ src/js/Actions/UIActions/RepositoryMuteFilters.ts
@@ -0,0 +1,53 @@
+import * as get from 'lodash/lodash';
+import { push } from 'react-router-redux';
+
+import { getState } from 'Helpers/State/Store';
+
+import { addRepository } from 'Actions/Repositories';
+import { setupRepositoryMuteFilter as setupRepositoryMuteFilterInStore } from 'Actions/RepositoryMuteFilters';
+
+/**
+ * @param  {number} accountId
+ * @param  {IGitHubRepository} repository
+ * @param  {string=null} redirect
+ */
+export function setupRepositoryMuteFilter(accountId: number,
+                                          repository: IGitHubRepository,
+                                          redirect: string = null)
+{
+  return dispatch =>
+  {
+    let canRedirect = typeof redirect === 'string';
+
+    let state          = getState<IState>();
+    let existingFilter = get(state.repositoryMuteFilters, `${accountId}.${repository.id}`, undefined);
+
+    /*
+     * If we have an existing filter than go no further.
+     * Although redirect if one has been set.
+     */
+    if (typeof existingFilter !== 'undefined') {
+      if (canRedirect) {
+        push(redirect);
+      }
+
+      return;
+    }
+
+    /*
+     * Store the repository in state, and setup the default filters
+     */
+    dispatch(addRepository(repository));
+    dispatch(setupRepositoryMuteFilterInStore(accountId, repository.id.toString()));
+
+    /*
+     * Make sure we've let things setup, then redirect
+     */
+    setTimeout(() =>
+    {
+      if (canRedirect) {
+        push(redirect);
+      }
+    }, 0);
+  };
+};
