c9b137d3b330fbb406824fee73f41e760fb7cc04
Andrew Hathaway <andrew@andrewhathaway.net>
2017-01-08 12:55:32 +0000

    Initial RoundedBtnSet UI component

---
+++ src/js/View/Ui/RoundedBtnSet.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+
+import { Btn } from './Index';
+
+interface IRoundedBtnSetProps
+{
+  className?: string;
+
+  buttons: IRoundedBtnSetBtn[];
+};
+
+interface IRoundedBtnSetBtn
+{
+  key: string;
+
+  text: string;
+
+  className?: string;
+
+  onClick?();
+};
+
+class RoundedBtnSet extends React.Component<IRoundedBtnSetProps, any>
+{
+  render()
+  {
+    // @todo: Array helper methods to clean this array logic up
+    return (
+      <div className={this.props.className}>
+        {this.props.buttons
+             .map((btn, i, a) =>
+             {
+               let className = (btn.className || '')
+                                  + (a.length > 0
+                                        && i === 0
+                                        ? ' btn--hard-bottom'
+                                        : '')
+                                  + (a.length > 0
+                                        && i + 1 === a.length
+                                        ? ' btn--hard-top'
+                                        : '')
+                                  + (a.length > 0
+                                      && i !== 0
+                                      && i + 1 < a.length
+                                      ? ' btn--hard'
+                                      : '')
+
+               return (
+                 <Btn key={btn.key}
+                      className={className}
+                      onClick={btn.onClick}>
+                  {btn.text}
+                </Btn>
+               );
+             })}
+      </div>
+    );
+  }
+};
+
+export default RoundedBtnSet;
